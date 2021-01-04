5581034baa06f4a39ffd51433c99c2da29825e55
Melissa-Karp <61592412+Melissa-Karp@users.noreply.github.com>
2021-01-04 17:57:48 -0500

    Update Fitting_GAM_noChl.R

--- Fitting_GAM_noChl.R
+++ Fitting_GAM_noChl.R
@@ -1,7 +1,7 @@
 ### Fishery-dependent SDM (L^3) Estimation Model Code
 ## Function to run GAMs on data from OM -- with missing chla covariate & some w/ spatial...
 ## Function returns only the fitted and predicted values
-#modified my M.Karp 12/15/20
+#modified my M.Karp 1/04/21
 
 ##################################
 #   Partial_models without Chla  #
@@ -11,6 +11,7 @@
 ##### Random sampling ####
 #
     if("ran" %in% sampling){
+      print("Fitting GAM-Ran_nochl")
       gam_Ran_P_nochl <- gam(pres~s(temp) + s(mld), data=dat_hist_random, family=binomial) 
       #plot(gam_Ran_P_nochl, pages=1)
       gam_Ran_N_nochl <- gam(log_abundance~s(temp)+ s(mld), data=dat_hist_random[dat_hist_random$abundance>0,], family=gaussian)
@@ -30,6 +31,7 @@
 #
     #Pref sampling - 0.5
     if("tar_0.5" %in% sampling) {
+      print("Fitting GAM-Pref0.5_nochl")
       gam_Tar_P_1_nochl <- gam(pres~s(temp)+ s(mld), data=dat_hist_Tar_1, family=binomial)
       #plot(gam_Tar_P_1_nochl, pages=1)
       gam_Tar_N_1_nochl <- gam(log_abundance~s(temp)+ s(mld), data=dat_hist_Tar_1[dat_hist_Tar_1$abundance>0,], family=gaussian)
@@ -45,6 +47,7 @@
     }
     #Pref sampling -0.6
     if("tar_0.6" %in% sampling) {
+      print("Fitting GAM-Pref0.6_nochl")
       gam_Tar_P_2_nochl <- gam(pres~s(temp)+ s(mld), data=dat_hist_Tar_2, family=binomial)
       #plot(gam_Tar_P_2_nochl, pages=1)
       gam_Tar_N_2_nochl <- gam(log_abundance~s(temp)+ s(mld), data=dat_hist_Tar_2[dat_hist_Tar_2$abundance>0,], family=gaussian)
@@ -60,6 +63,7 @@
     }
     #Pref sampling -0.7
     if("tar_0.7" %in% sampling) {
+      print("Fitting GAM-Pref0.7_nochl")
       gam_Tar_P_3_nochl <- gam(pres~s(temp)+ s(mld), data=dat_hist_Tar_3, family=binomial)
       #plot(gam_Tar_P_3_nochl, pages=1)
       gam_Tar_N_3_nochl <- gam(log_abundance~s(temp)+ s(mld), data=dat_hist_Tar_3[dat_hist_Tar_3$abundance>0,], family=gaussian)
@@ -75,6 +79,7 @@
     }
     #Pref sampling -0.8
     if("tar_0.8" %in% sampling) {
+      print("Fitting GAM-Pref0.8_nochl")
       gam_Tar_P_4_nochl <- gam(pres~s(temp)+ s(mld), data=dat_hist_Tar_4, family=binomial)
       #plot(gam_Tar_P_4_nochl, pages=1)
       gam_Tar_N_4_nochl <- gam(log_abundance~s(temp)+ s(mld), data=dat_hist_Tar_4[dat_hist_Tar_4$abundance>0,], family=gaussian)
@@ -90,6 +95,7 @@
     }
     #Pref sampling -0.9
     if("tar_0.9" %in% sampling) {
+      print("Fitting GAM-Pref0.9_nochl")
       gam_Tar_P_5_nochl <- gam(pres~s(temp)+ s(mld), data=dat_hist_Tar_5, family=binomial)
       #plot(gam_Tar_P_5_nochl, pages=1)
       gam_Tar_N_5_nochl <- gam(log_abundance~s(temp)+ s(mld), data=dat_hist_Tar_5[dat_hist_Tar_5$abundance>0,], family=gaussian)
@@ -109,6 +115,7 @@
 #
     #Dist sampling - NPO
     if("npo" %in% sampling){
+      print("Fitting GAM-NPO_nochl")
       gam_Dist_P_npo_nochl <- gam(pres~s(temp) + s(mld), data=dat_hist_Dist_npo, family=binomial)
       #plot(gam_Dist_P_npo_nochl, pages=1)
       gam_Dist_N_npo_nochl <- gam(log_abundance~s(temp) + s(mld), data=dat_hist_Dist_npo[dat_hist_Dist_npo$abundance>0,], family=gaussian)
@@ -124,6 +131,7 @@
     }
     #Dist sampling - NPN
     if("npn" %in% sampling){
+      print("Fitting GAM-NPN_nochl")
       gam_Dist_P_npn_nochl <- gam(pres~s(temp) + s(mld), data=dat_hist_Dist_npn, family=binomial)
       #plot(gam_Dist_P_npn_nochl, pages=1)
       gam_Dist_N_npn_nochl <- gam(log_abundance~s(temp) + s(mld), data=dat_hist_Dist_npn[dat_hist_Dist_npn$abundance>0,], family=gaussian)
@@ -139,6 +147,7 @@
     }
     #Dist sampling - MPO
     if("mpo" %in% sampling){
+      print("Fitting GAM-MPO_nochl")
       gam_Dist_P_mpo_nochl <- gam(pres~s(temp) + s(mld), data=dat_hist_Dist_mpo, family=binomial)
       #plot(gam_Dist_P_mpo_nochl, pages=1)
       gam_Dist_N_mpo_nochl <- gam(log_abundance~s(temp) + s(mld), data=dat_hist_Dist_mpo[dat_hist_Dist_mpo$abundance>0,], family=gaussian)
@@ -154,6 +163,7 @@
     }
     #Dist sampling - MPN
     if("mpn" %in% sampling){
+      print("Fitting GAM-NPN_nochl")
       gam_Dist_P_mpn_nochl <- gam(pres~s(temp) + s(mld), data=dat_hist_Dist_mpn, family=binomial)
       #plot(gam_Dist_P_mpn_nochl, pages=1)
       gam_Dist_N_mpn_nochl <- gam(log_abundance~s(temp) + s(mld), data=dat_hist_Dist_mpn[dat_hist_Dist_mpn$abundance>0,], family=gaussian)
@@ -169,6 +179,7 @@
     }
     #Dist sampling - SPO
     if("spo" %in% sampling){
+      print("Fitting GAM-SPO_nochl")
       gam_Dist_P_spo_nochl <- gam(pres~s(temp) + s(mld), data=dat_hist_Dist_spo, family=binomial)
       #plot(gam_Dist_P_spo_nochl, pages=1)
       gam_Dist_N_spo_nochl <- gam(log_abundance~s(temp) + s(mld), data=dat_hist_Dist_spo[dat_hist_Dist_spo$abundance>0,], family=gaussian)
@@ -184,6 +195,7 @@
     }
     #Dist sampling - SPN
     if("spn" %in% sampling){
+      print("Fitting GAM-SPN_nochl")
       gam_Dist_P_spn_nochl <- gam(pres~s(temp) + s(mld), data=dat_hist_Dist_spn, family=binomial)
       #plot(gam_Dist_P_spn_nochl, pages=1)
       gam_Dist_N_spn_nochl <- gam(log_abundance~s(temp) + s(mld), data=dat_hist_Dist_spn[dat_hist_Dist_spn$abundance>0,], family=gaussian)
@@ -199,6 +211,7 @@
     }
     #Dist sampling - ALLO
     if("allo" %in% sampling){
+      print("Fitting GAM-ALLO_nochl")
       gam_Dist_P_allo_nochl <- gam(pres~s(temp) + s(mld), data=dat_hist_Dist_allo, family=binomial)
       #plot(gam_Dist_P_allo_nochl, pages=1)
       gam_Dist_N_allo_nochl <- gam(log_abundance~s(temp) + s(mld), data=dat_hist_Dist_allo[dat_hist_Dist_allo$abundance>0,], family=gaussian)
@@ -214,6 +227,7 @@
     }
     #Dist sampling - Alln
     if("alln" %in% sampling){
+      print("Fitting GAM-ALLN_nochl")
       gam_Dist_P_alln_nochl <- gam(pres~s(temp) + s(mld), data=dat_hist_Dist_alln, family=binomial)
       #plot(gam_Dist_P_alln_nochl, pages=1)
       gam_Dist_N_alln_nochl<- gam(log_abundance~s(temp) + s(mld), data=dat_hist_Dist_alln[dat_hist_Dist_alln$abundance>0,], family=gaussian)
@@ -226,6 +240,77 @@
       dat_fcast$presxD_alln_nochl <- predict(gam_Dist_P_alln, dat_fcast, type="response")
       abundxD_alln_nochl <- predict(gam_Dist_N_alln, dat_fcast, type="response")
       dat_fcast$gam_Dist_alln_nochl <- dat_fcast$presxD_alln_nochl * exp(abundxD_alln_nochl)
+    }
+    
+#
+######BY sampling
+#
+    if("BY" %in% sampling){
+      print("Fitting GAM-BY_nochl")
+      gam_BY_P_nochl <- gam(pres~s(temp) + s(mld), data=dat_hist_BY, family=binomial)
+      #plot(gam_BY_P_nochl, pages=1)
+      gam_BY_N_nochl<- gam(log_abundance~s(temp) + s(mld), data=dat_hist_BY[dat_hist_BY$abundance>0,], family=gaussian)
+      #plot(gam_BY_N_nochl, pages=1)
+      
+      dat_hist$presxB_nochl <- predict(gam_BY_P_nochl, dat_hist, type="response")
+      abundxB_nochl <- predict(gam_BY_N_nochl, dat_hist, type="response")
+      dat_hist$gam_BY_nochl <- dat_hist$presxB_nochl * exp(abundxB_nochl)
+      
+      dat_fcast$presxB_nochl <- predict(gam_BY_P_nochl, dat_fcast, type="response")
+      abundxB_nochl <- predict(gam_BY_N_nochl, dat_fcast, type="response")
+      dat_fcast$gam_BY_nochl <- dat_fcast$presxB_nochl * exp(abundxB_nochl)
+    }
+    
+#
+#####Closed Area Sampling
+#
+    #Closed Area - Small
+    if("CA_sm" %in% sampling) {
+      print("Fitting GAM-CASM_nochl")
+      gam_CA_P_sm_nochl <- gam(pres~s(temp) + s(mld), data=dat_hist_CA_sm, family=binomial)
+      #plot(gam_CA_P_sm_nochl, pages=1)
+      gam_CA_N_sm_nochl <- gam(log_abundance~s(temp) + s(mld), data=dat_hist_CA_sm[dat_hist_CA_sm$abundance>0,], family=gaussian)
+      #plot(gam_CA_N_sm_nochl, pages=1)
+      
+      dat_hist$presxCA_sm_nochl <- predict(gam_CA_P_sm_nochl, dat_hist, type="response")
+      abundxCA_sm_nochl <- predict(gam_CA_N_sm_nochl, dat_hist, type="response")
+      dat_hist$gam_CA_sm_nochl <- dat_hist$presxCA_sm_nochl * exp(abundxCA_sm_nochl)
+      
+      dat_fcast$presxCA_sm_nochl <- predict(gam_CA_P_sm_nochl, dat_fcast, type="response")
+      abundxCA_sm_nochl <- predict(gam_CA_N_sm_nochl, dat_fcast, type="response")
+      dat_fcast$gam_CA_sm_nochl <- dat_fcast$presxCA_sm_nochl * exp(abundxCA_sm_nochl)
+    }
+    #Closed Area - Medium
+    if("CA_med" %in% sampling) {
+      print("Fitting GAM-CAMED_nochl")
+      gam_CA_P_med_nochl <- gam(pres~s(temp) + s(mld), data=dat_hist_CA_med, family=binomial)
+      #plot(gam_CA_P_med_nochl, pages=1)
+      gam_CA_N_med_nochl <- gam(log_abundance~s(temp) + s(mld), data=dat_hist_CA_med[dat_hist_CA_med$abundance>0,], family=gaussian)
+      #plot(gam_CA_N_med_nochl, pages=1)
+      
+      dat_hist$presxCA_med_nochl <- predict(gam_CA_P_med_nochl, dat_hist, type="response")
+      abundxCA_med_nochl <- predict(gam_CA_N_med_nochl, dat_hist, type="response")
+      dat_hist$gam_CA_med_nochl <- dat_hist$presxCA_med_nochl * exp(abundxCA_med_nochl)
+      
+      dat_fcast$presxCA_med_nochl <- predict(gam_CA_P_med_nochl, dat_fcast, type="response")
+      abundxCA_med_nochl <- predict(gam_CA_N_med_nochl, dat_fcast, type="response")
+      dat_fcast$gam_CA_med_nochl <- dat_fcast$presxCA_med_nochl * exp(abundxCA_med_nochl)
+    }
+    #Closed Area - Large
+    if("CA_lar" %in% sampling) {
+      print("Fitting GAM-CALAR_nochl")
+      gam_CA_P_lar_nochl <- gam(pres~s(temp) + s(mld), data=dat_hist_CA_lar, family=binomial)
+      #plot(gam_CA_P_lar_nochl, pages=1)
+      gam_CA_N_lar_nochl <- gam(log_abundance~s(temp) + s(mld), data=dat_hist_CA_lar[dat_hist_CA_lar$abundance>0,], family=gaussian)
+      #plot(gam_CA_N_lar_nochl, pages=1)
+      
+      dat_hist$presxCA_lar_nochl <- predict(gam_CA_P_lar, dat_hist, type="response")
+      abundxCA_lar_nochl <- predict(gam_CA_N_lar, dat_hist, type="response")
+      dat_hist$gam_CA_lar_nochl <- dat_hist$presxCA_lar_nochl * exp(abundxCA_lar_nochl)
+      
+      dat_fcast$presxCA_lar_nochl <- predict(gam_CA_P_lar_nochl, dat_fcast, type="response")
+      abundxCA_lar_nochl <- predict(gam_CA_N_lar_nochl, dat_fcast, type="response")
+      dat_fcast$gam_CA_lar_nochl <- dat_fcast$presxCA_lar_nochl * exp(abundxCA_lar_nochl)
     }
     
 #
